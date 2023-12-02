a7ffce586d56995f59c0939391b169fabfe9eb11
supershem24 <super.sj24@gmail.com>
2023-12-02 15:58:43 -0500

    Added a bit of functionality to the Js and a bit more progress to the actual website.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // JavaScript source code
+
+let imageHolder = document.getElementsByClassName("ImageHolder");
+let rightButton = document.getElementsByClassName("RightButton");
+let leftButton = document.getElementsByClassName("LeftButton");
+
+for (let i = 0; i < rightButton.length; i++) {
+
+    if (i % 3 == 0) {
+
+    }
+    imageHolder[i].
+}
+
+for (let i = 0; i < rightButton.length; i++) {
+    rightButton[i].addEventListener("click", () => {
+        imageHolder[i].scrollLeft += 900;
+    });
+}
+
+for (let i = 0; i < leftButton.length; i++) {
+    leftButton[i].addEventListener("click", () => {
+        imageHolder[i].scrollLeft -= 900;
+    });
+}
